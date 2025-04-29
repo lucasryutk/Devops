80408f8e5df6c2d75ea2cd4f79e8f1d439f949ca
lucasryutk <lucasryutaka@gmail.com>
2025-04-28 21:00:37 -0300

    test

--- test/test.py
+++ test/test.py
@@ -1,25 +1,40 @@
 from src.main import *
 from unittest.mock import patch
+
+
 def test_root():
-    assert root() =={"message": "Hello World"}
+    result = root()
+    yield result
+    assert result == {"message": "Hello World"}
 
 def test_funcaoteste():
     with patch('random.randint', return_value=12345):
         result = funcaoteste()
-    assert result == {"message": True, "num_aleatorio": 13245}
+        yield result
+    assert result == {"teste": True, "num_aleatorio": 13245}
 
 def test_create_estudante(estudante: Estudante):
     estudante_teste = Estudante(name="Lucas", curso="ADS", ativo=False)
-    assert estudante_teste == create_estudante()
+    result = create_estudante(estudante_teste)
+    yield result
+    assert estudante_teste == result
 
 def test_update_estudante_negativo(id_estudante: int):
-    assert not update_estudante(-1)
+    result = update_estudante(-5)
+    yield result
+    assert not result
 
 def test_update_estudante_positivo(id_estudante: int):
-    assert not update_estudante(1)
+    result = update_estudante(1)
+    yield update_estudante(1)
+    assert not result
 
 def test_delete_estudante_negativo(id_estudante: int):
-    assert not delete_estudante(-1)
+    result = delete_estudante(-1)
+    yield result
+    assert not result
 
 def test_delete_estudante_positivo(id_estudante: int):
-    assert not delete_estudante(1)
+    result = delete_estudante(-1)
+    yield result
+    assert not result
