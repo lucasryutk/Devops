e4d25efebb8d1b025ed81e95ca55e98167c75363
lucasryutk <lucasryutaka@gmail.com>
2025-04-28 21:24:55 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -16,28 +16,28 @@ async def test_funcaoteste():
     assert result == {"message": True, "num_aleatorio": 12345}
 
 @pytest.mark.asyncio
-async def test_create_estudante(estudante: Estudante):
+async def test_create_estudante():
     estudante_teste = Estudante(name="Lucas", curso="ADS", ativo=False)
     result = await create_estudante(estudante_teste)
     assert estudante_teste == result
 
 @pytest.mark.asyncio
-async def test_update_estudante_negativo(id_estudante: int):
+async def test_update_estudante_negativo():
     result = await update_estudante(-5)
     assert not result
 
 @pytest.mark.asyncio
-async def test_update_estudante_positivo(id_estudante: int):
+async def test_update_estudante_positivo():
     result = await update_estudante(1)
     assert not result
 
 @pytest.mark.asyncio
-async def test_delete_estudante_negativo(id_estudante: int):
+async def test_delete_estudante_negativo():
     result = await delete_estudante(-1)
     yield result
     assert not result
 
 @pytest.mark.asyncio
-async def test_delete_estudante_positivo(id_estudante: int):
+async def test_delete_estudante_positivo():
     result = await delete_estudante(-1)
     assert not result
